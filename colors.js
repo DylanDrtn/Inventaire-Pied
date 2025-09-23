4fadb1a2e1921ff517975f0527544586e03603dd
DylanDrtn <dylan.drtn@gmail.com>
2025-09-24 01:30:37 +0200

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,42 @@
+// Dictionnaire interne Fermob (2025-2026)
+export const COLOR_HEX = {
+  "blanc coton":"#F1F2F3",
+  "gris orage":"#6D6E71",
+  "gris argile":"#9C9B95",
+  "gris lapili":"#BEBDB8",
+  "gris lapilli":"#BEBDB8",
+
+  "carbone":"#2B2B2B",
+  "anthracite":"#474747",
+  "réglisse":"#2B2B2C",
+
+  "bleu abysse":"#0E3A5B",
+  "bleu abysses":"#0E3A5B",
+  "bleu acapulco":"#2F6F9E",
+  "bleu maya":"#2B4DA3",
+
+  "cactus":"#5A7D3A",
+  "romarin":"#8AA38B",
+  "rouille":"#A34E2E",
+  "piment":"#B72227",
+  "capucine":"#E85C41",
+  "cerise noire":"#61112A",
+  "beige latte":"#D7C9B8",
+  "verveine":"#C7E15E",
+  "olive":"#6D7C3A",
+  "muscat":"#A4B500",
+  "miel":"#C08A2C",
+  "noisette":"#8E6B3D",
+
+  "opaline":"#BBD6D6",
+  "guimauve":"#E6C4D3",
+  "rose praline":"#E37A8E",
+
+  "orange confite":"#9C4B1C",
+  "orange confit":"#9C4B1C",
+  "citron givré":"#E6E879",
+
+  "pain d'épices":"#986F31",
+  "pain d’epices":"#986F31",
+  "pain d''epices":"#986F31"
+};
